4cebb7acecfd593ade39922aa3826c1ea1cc4952
Hans Guenther <hgconcept.art@gmail.com>
2024-09-17 14:54:56 -0400

    Episode 0 to 7

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,71 @@
 <?php
 
+/*
+ *	Routes take a url input, and return something something else.
+ *	This could be a file, or even a string.
+ */
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('jobs', function() {
+    return view('jobs',
+		[
+		'jobs' =>
+			[
+				[
+				'id' => 0,
+				'title' => 'Director',
+				'salary' => '$50,000'
+				],
+				[
+				'id' => 1,
+				'title' => 'Programmer',
+				'salary' => '$10,000'
+				],
+				[
+				'id' => 2,
+				'title' => 'Teacher',
+				'salary' => '$40,000'
+				]
+			]
+		]
+	);
+});
+
+Route::get('/jobs/{id}', function($id) {
+	$jobs = [
+		[
+		'id' => 0,
+		'title' => 'Director',
+		'salary' => '$50,000'
+		],
+		[
+		'id' => 1,
+		'title' => 'Programmer',
+		'salary' => '$10,000'
+		],
+		[
+		'id' => 2,
+		'title' => 'Teacher',
+		'salary' => '$40,000'
+		]
+	];
+
+	$job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+	
+	return view('job', ['job' => $job]);
+});
+
+Route::get('/about', function() {
+	return view('about');
+});
+
+Route::get('/contact', function() {
+	return view('contact');
+});
+
+
