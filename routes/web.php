da6c8c20a1b6031fe189ce3b0220cbabb321629d
Hans Guenther <hgconcept.art@gmail.com>
2024-09-21 22:28:51 -0400

    Finished video 21

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,10 @@
  *	Routes take a url input, and return something something else.
  *	This could be a file, or even a string.
  */
+
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 // Main
@@ -12,3 +15,10 @@ Route::view('/', 'home');
 Route::view('/about', 'about');
 Route::view('/contact', 'contact');
 Route::resource('jobs', JobController::class);
+
+// Auth
+Route::get('/register', [RegisterUserController::class, 'create']);
+Route::post('/register', [RegisterUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
